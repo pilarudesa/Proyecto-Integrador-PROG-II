50046a547aad16d45efd9c2ca0b51b18806b9dfe
pilarudesa <pilargomez@udesa.edu.ar>
2025-04-12 15:31:35 -0300

    realice el formulario de login, faltaria cambiar el action a su respectiva ruta. cree el objeto literal con propiedades de users, productos y comentarios de forma generica.

--- db/dbproducts.js
+++ db/dbproducts.js
@@ -1,23 +1,342 @@
 /* base de datos inventada, no hace falta requerir express aca, es un modulo (obj lit)*/
 
-const products = {
-    lista: [
+const data = {
+    users: [
+
+    ],
+    productos: [
       {
         id: 1,
         nombre: "Zapatillas Urbanas",
         descripcion: "Zapatillas cómodas para uso diario.",
         imagen: "/images/zapatillas.jpg",
-        comentarios: ["Muy cómodas", "Me gustaron mucho"]
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
       },
       {
         id: 2,
         nombre: "Mochila Compacta",
         descripcion: "Ideal para llevar lo esencial.",
         imagen: "/images/mochila.jpg",
-        comentarios: ["Buena calidad", "La uso todos los días"]
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 3,
+        nombre: "Cafetera Moulinex",
+        descripcion: "Ideal para el cafe de todas las mañanas",
+        imagen: "/images/products/cafetera-moulinex.jpg",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 4,
+        nombre: "Computadora Macbook Pro",
+        descripcion: "Modelo 2019 en perfecto estado",
+        imagen: "/images/products/macbook-pro-2019.jpg",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 5,
+        nombre: "Celular Samsung Galaxy s10",
+        descripcion: "Celular de ultima generacion",
+        imagen: "/image/products/samsung-galaxy-s10.jpg",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 6,
+        nombre: "Televisor Samsung Smart",
+        descripcion: "Televisor de 150 pulgadas",
+        imagen: "/image/products/tv-samsung-smart.jpg",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 7,
+        nombre: "",
+        descripcion: "",
+        imagen: "/image/products/",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 8,
+        nombre: "",
+        descripcion: "",
+        imagen: "/image/products/",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 9,
+        nombre: "",
+        descripcion: "",
+        imagen: "/image/products/",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
+      },
+      {
+        id: 10,
+        nombre: "",
+        descripcion: "",
+        imagen: "/image/products/",
+        comentarios: [
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          },
+          {
+            nombreUsuario: "MartinaP",
+            textoComentario: "Muy cómodas, me gustaron mucho",
+            imagenPerfil: ""
+          }
+        ]
       }
     ]
   };
 
-module.exports = products;
+module.exports = data;
 
